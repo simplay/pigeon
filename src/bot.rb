a2e432f02f6b52900930a2dec4782a15df63737c
simplay <silent.simplay@gmail.com>
2015-10-30 17:44:03 +0100

    Added some simple basic commands

--- src/bot.rb
+++ src/bot.rb
@@ -1,11 +1,14 @@
 require 'java'
+require 'pry'
 require 'lib/teamspeak3-api-1.0.12.jar'
 java_import 'com.github.theholywaffle.teamspeak3.TS3Query'
 java_import 'com.github.theholywaffle.teamspeak3.TS3Api'
+java_import "com.github.theholywaffle.teamspeak3.api.event.TS3Listener"
 class Bot
-
-  def initialize(config)
+  MASTER_NAME = "simplay"
+  def initialize(config, name="Sir Pigeon")
     @query = TS3Query.new(config.data)
+    @name = name
     @is_started = false
   end
 
@@ -15,7 +18,8 @@ class Bot
       @query.connect
       @api = @query.getApi
       @api.selectVirtualServerById(1)
-      @api.setNickname("Sir Pigeon")
+      @api.setNickname(@name)
+      attach_listeners
     end
   end
 
@@ -32,5 +36,30 @@ class Bot
 
   def say_in_current_channel(msg)
     @api.sendChannelMessage(msg)
+  end
+
+  protected
+
+  def attach_listeners
+    @api.registerAllEvents
+    @api.addTS3Listeners TS3Listener.impl {|name, event|
+      unless event.getInvokerName.include?(@name)
+        if name.to_s.include?("onTextMessage")
+          case name.to_s
+          when 'onTextMessage'
+            sender_name = event.getInvokerName
+            if sender_name.downcase.include?(MASTER_NAME)
+              if event.getMessage == "!bb"
+                say_in_current_channel("I'm leaving now - cu <3")
+                shut_down
+              end
+            end
+            if event.getMessage == "!poke"
+              say_in_current_channel("Hey, stop poking me!")
+            end
+          end
+        end
+      end
+    }
   end
 end
--- src/bot_manager.rb
+++ src/bot_manager.rb
@@ -4,9 +4,7 @@ require_relative 'server_config'
 class BotManager
   def initialize
     config = ServerConfig.new
-    bot = Bot.new(config)
+    bot = Bot.new(config, "Pigeon")
     bot.start
-    bot.say_in_current_channel("pew nils pew pew pew ...")
-    bot.shut_down
   end
 end
