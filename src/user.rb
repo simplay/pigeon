844cd6734d8470842244130d3821ae34cb7903fd
simplay <silent.simplay@gmail.com>
2015-11-05 11:52:09 +0100

    Extending docs of User class

--- src/user.rb
+++ src/user.rb
@@ -1,6 +1,15 @@
+# A User models a ts3 client including its permission levels,
+# the list of server groups a user belongs to, and his nickname.
+# We use the notion of users to check for authorization when
+# invoking a command. Furthermore it allows to map data to users
+# E.g. the logic behing the '!ll' command.
 class User
+
   attr_reader :id, :nick
 
+  # List all online users.
+  #
+  # @return [Array<User>] list of all users currently online on this server.
   def self.all
     server_groups = Server.groups
     @users = Server.api.get_clients.map do |client|
@@ -11,51 +20,89 @@ class User
     end
   end
 
+  # Try to find a user with a given nick name.
+  #
+  # @param nick [String] target user's nickname.
+  # @info: This query tries to fully match a user's nickname
+  #   with a provided nick name string. Thus, partial matches will
+  #   result in a missmatch.
+  #   Note that the matcher is case-sensitive.
+  # @example
+  #   # say there is a user called Foo currently
+  #   User.find_by_nick("Foo")
+  #   #=> #<User:0x30c86ea3 @id=1, @levels={6=>"Server Admin"}, @nick="Foo">
+  #
+  #   User.find_by_nick("foo")
+  #   #=> nil
+  #
+  # @return [User, nil] user with given name
+  #   or nil, if no such user is online.
   def self.find_by_nick(nick)
     all.find {|user| user.nick==nick}
   end
 
+  # List all fetched users previousely fetched via #all
+  #
+  # @info: in case User#all was not previousely invoked,
+  #   it will return nil
+  # @return [Array<User>, nil] list of users previousely fetched.
   def self.users
     @users
   end
 
-  def initialize(id, nick, lvls)
-    @nick = nick
-    @levels = lvls
+  # @param id [Integer] corresponds to client id in ts3 db.
+  # @param nick [String] corresponds to client nick name in ts3 db.
+  # @param lvls [Hash{ServerGroupId=>ServerGroupName}] list of user's
+  #   ts3 permissions.
+  def initialize(id, nick, permissions)
     @id = id
+    @nick = nick
+    @levels = permissions
   end
 
-  def level?(cmp_lvl)
+  # Checks whether this user the required permissings.
+  #
+  # @info: TS3 convention: Lower level values mean higher permission levels.
+  #   This means that a low level value corresponds to a very priviliged user
+  #   and a high user level corresponds to a low user level
+  #   (i.e. a user with only a few rights).
+  # @param required_level [Integer] minimal required permission level.
+  # @return [Boolean] true if this user's permission level is
+  #   meets the required permission level.
+  def level?(required_level)
     top_lvl = levels.keys.min
-    top_lvl <= cmp_lvl
+    top_lvl <= required_level
   end
 
+  # List of user's permissons.
+  #
+  # @info: the lower the server group id, the higher the permission level.
+  # @return [Hash{ServerGroupId=>ServerGroupName}] list of user's
   def levels
     @levels
   end
 
+  # Checks whether this user is a bot.
+  #
+  # @info: Every user that belongs to the server group
+  #   "Admin Server Query" represents a Bot.
+  # @return [Boolean] true if bot otherwise false.
   def bot?
     @levels.values.include?("Admin Server Query")
   end
 
-  def try_run_command(invoking_command)
-    invoking_command.invoke_by(self)
-  end
-
   protected
 
+  # Forms a Hash from two given equally sized arrays.
+  #
+  # @info: First array depicts the list of hash keys,
+  #   and the second array the hash values.
+  # @param ids [Array<Integer>] server group ids.
+  # @param names [Array<String>] server group names.
+  # @return [Hash{ServerGroupId=>ServerGroupName}] permission list.
   def self.group_id_names_hash(ids, names)
       levels = ids.zip(names)
       Hash[*levels.flatten]
   end
 
-  # Obtain a unique user id
-  # @info: first id value is equal zero.
-  #   id values are increased by one,
-  #   whenever this method is called.
-  # @return [Integer] unique user id.
-  def next_id
-    @@global_u_id ||= 0
-    @@global_u_id = @@global_u_id + 1
-  end
 end
