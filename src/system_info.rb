4a1396720262bff745943012b1a9852ce8739dca
simplay <silent.simplay@gmail.com>
2015-12-05 14:37:15 +0100

    Allow to query for system runtime information

---
+++ src/system_info.rb
@@ -0,0 +1,60 @@
+require 'java'
+java_import 'java.lang.System'
+
+# SystemInformation provides runtime information such as on which os Pigeon is running
+# or by whom it is called (either from the console or by an exectuable jar).
+class SystemInfo
+
+  attr_reader :os, :caller
+
+  CALLER_JAR = 'jar'
+  CALLER_CONSOLE = 'console'
+
+  def self.singleton
+    @instance ||= SystemInfo.new
+  end
+
+  # Does bofrev run on a mac?
+  #
+  # @return [Boolean] true if bofrev is run on a mac os and false otherwise.
+  def self.running_on_mac?
+    singleton.os.include?('mac')
+  end
+
+  # Does bofrev run on a windows os?
+  #
+  # @return [Boolean] true if bofrev is run on a windows os and false otherwise.
+  def self.running_on_windows?
+    singleton.os.include?('windows')
+  end
+
+  # Does bofrev run on a linux os?
+  #
+  # @return [Boolean] true if bofrev is run on a linux os and false otherwise.
+  def self.running_on_linux?
+    singleton.os.include?('linux')
+  end
+
+  # Is bofrev called by an exectuable jar?
+  #
+  # @return [Boolean] true if bofrev is called by its exectuable jar and false otherwise.
+  def self.called_by_jar?
+    singleton.caller == CALLER_JAR
+  end
+
+  # Is bofrev called from a console?
+  #
+  # @return [Boolean] true if bofrev is called within the console and false otherwise.
+  def self.called_by_console?
+    singleton.caller == CALLER_CONSOLE
+  end
+
+  private
+
+  def initialize
+    caller_name = "#{$PROGRAM_NAME}"
+    @caller = caller_name.include?("<script>") ? CALLER_JAR : CALLER_CONSOLE
+    @os = System.getProperty("os.name").downcase
+  end
+
+end
