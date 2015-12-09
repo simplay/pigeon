e8ae8c5c7aa0ef74374e5815a76b7ae4c11d84c3
simplay <silent.simplay@gmail.com>
2015-12-09 21:28:20 +0100

    Minor refactoring and extended docs of TextBlock class

--- src/text_block.rb
+++ src/text_block.rb
@@ -1,4 +1,10 @@
-# Linked Datastructure to concatinate text blocks
+# TextBlock instances are chained structures containing formatted texts.
+#
+# Every TextBlock has some kind of content, that is parsed during initialization
+# and an optional successor.
+# A text block responds to #to_s which recursively triggers the
+# to_s on self and its successor.
+#
 # Every text element managed, edited or created by the bot
 # is supposed to be a TextBlock
 #
@@ -19,15 +25,24 @@ class TextBlock
     @content = parse(content)
   end
 
+  # Append a successor block.
+  #
+  # @info: When rendering text block instances, the successor block is rendered
+  #   right after this one.
+  # @param block [< TextBlock] the successor block of this one.
   def append_successor(block)
     @successor = block
     block.predecessor = self
   end
 
+  # Does this block exhibit a successor block?
+  #
+  # @return [Boolean] true if it has a successor, otherwise false.
   def successor?
     !@successor.nil?
   end
 
+  # Expand recursively the content of this text block
   def to_s
     if successor?
       return @content + @successor.to_s
@@ -38,12 +53,16 @@ class TextBlock
 
   protected
 
+  # Make a printable String from the given input message.
   def parse(message)
-    message
+    message.to_s
   end
 
 end
 
+# @example
+#   BoldText.new("very bold text")
+#   #=> #<BoldText:0x6933b6c6 @content="[b]very bold text[/b]">
 class BoldText < TextBlock
   def parse(message)
     "[b]#{message}[\/b]"
@@ -51,7 +70,7 @@ class BoldText < TextBlock
 end
 
 # @example
-#   TextLink.new("www.google.coom").to_s
+#   LinkText.new("www.google.coom").to_s
 #   #=> "[URL]www.google.coom[/URL]"
 class LinkText < TextBlock
 
@@ -61,11 +80,21 @@ class LinkText < TextBlock
 
 end
 
+# @example
+#   LinedText.new(["1","2","3"]).to_s
+#   #=> "[b]1[/b] \n[b]2[/b] \n[b]3[/b] \n""
+#
+#   LinedText.new("3").to_s
+#   #=> "3"
+#
+#   LinedText.new("3").to_s
+#   #=> "3"
 class LinedText < TextBlock
 
   def parse(message)
     return process_item(message) if message.is_a?(String)
-    (message.count > 1) ? process_list(message) : process_item(message)
+    return process_list(message) if message.is_a? Array
+    process_item(message)
   end
 
   protected
@@ -79,10 +108,24 @@ class LinedText < TextBlock
   end
 
   def process_item(msg)
-    "#{msg}"
+    "#{msg.to_s}"
   end
 end
 
+# @example
+#   ListText.new("Item").to_s
+#   # => "[list][*]Item[/list]"
+#
+#   ListText.new([1,2,3]).to_s
+#   #=> "[list]\n[*]1[*]2[*]3[/list]"
+#
+#   ListText.new(["foo","bar","baz"]).to_s
+#   #=> "[list]\n[*]foo[*]bar[*]baz[/list]"
+#
+#   t1 = LabeledLinkText.new("www.pow.coom", "foobar")
+#   t2 = LabeledLinkText.new("www.pew.coom", "baz")
+#   ListText.new([t1,t2]).to_s
+#   #=> "[list]\n[*][b]foobar[/b]:[URL]www.pew.coom[/URL][*][b]baz[/b]:[URL]www.pew.coom[/URL][/list]"]"
 class ListText < LinedText
 
   def process_list(msgs)
@@ -93,15 +136,14 @@ class ListText < LinedText
   end
 
   def process_item(msg)
-    txt = (msg.count == 1)? msg.first : msg
-    "[list][*]#{txt.to_s}[\/list]"
+    "[list][*]#{msg.to_s}[\/list]"
   end
 
 end
 
 # @example
-#   LabeledTextLink.new("www.google.coom", "foobar").to_s
-#   #=> "foobar: [URL]www.google.coom[/URL]"
+#   LabeledLinkText.new("www.google.coom", "foobar").to_s
+#   #=> "[b]foobar[/b]:[URL]www.google.coom[/URL]""
 class LabeledLinkText < LinkText
   def initialize(content, header, delimiter=":")
     @header = BoldText.new(header).to_s
