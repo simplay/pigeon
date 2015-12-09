26af05961030c0f77bbbb081aceb9d4f8f7e75bf
simplay <silent.simplay@gmail.com>
2015-12-09 20:57:31 +0100

    Wrote docs for DescriptionLinkStore

--- src/description_link_store.rb
+++ src/description_link_store.rb
@@ -1,5 +1,9 @@
 require 'pstore'
 
+# DescriptionLinkStore stores all links - mainly LabeledLinkText instances -
+# shown in a target channel description.
+# They are used in a particular Layout instance. Such links are managed by a unique identifier.
+# E.g. all minecraft Links are prefixed by "mc_".
 class DescriptionLinkStore
 
   def self.instance
@@ -26,6 +30,17 @@ class DescriptionLinkStore
     instance.find_all_including_key(name, return_key_value_pair)
   end
 
+  # Retrieve all entities keys which have a match between their identifier and a provided name.
+  #
+  # @example
+  #   #find_all_including_key("mc", true)
+  #   returns LabeledLinkText that match mc plus their identifiers.
+  #
+  # @info: Note that by default only their identifier is returned, by passing
+  #   return_key_value_pair = true, the whole entity will be returned.
+  # @param key_name [String] substring used for matching db entitiy ids.
+  # @return_key_value_pair [Boolean] optional, should the whole entity be returned.
+  # @return [Array] list of entities that match the input.
   def find_all_including_key(key_name, return_key_value_pair=false)
     s = all_stored.select do |link_node|
       link_node.to_s.include? key_name
@@ -34,6 +49,9 @@ class DescriptionLinkStore
     s.map(&:last)
   end
 
+  # Return every stored entity.
+  #
+  # @return [Array<ID, Array<LabeledLinkText.new>>] list of entities.
   def all_stored
     store.transaction do
       store.roots.map do |link|
@@ -42,6 +60,14 @@ class DescriptionLinkStore
     end
   end
 
+  # Store or update a link in the db that has an given id.
+  #
+  # @example
+  #   link = LabeledLinkText.new("Foobar", "www.foobar.com")
+  #   DescriptionLinkStore.write(link, :foobar_id)
+  #
+  # @param link [LabeledLinkText] a text link
+  # @param id [Symbol] identifier of this link element.
   def write(link, id)
     store.transaction do
       store[id] = link
@@ -49,6 +75,12 @@ class DescriptionLinkStore
     end
   end
 
+  # Delete a link by id.
+  #
+  # @example
+  #   DescriptionLinkStore.delete(:foobar_id)
+  #
+  # @param id [Symbol] identifier of an existing entity.
   def delete(id)
     store.transaction do
       store.delete(id)
@@ -56,6 +88,9 @@ class DescriptionLinkStore
     end
   end
 
+  # Fetch a entity by its identifier.
+  #
+  # @return [LabeledLinkText, nil] returns a stored link in case it exists.
   def find(id)
     store.transaction do
       begin
@@ -66,6 +101,7 @@ class DescriptionLinkStore
     end
   end
 
+  # Returns the file db.
   def store
     @file_db ||= PStore.new('data/desc_links.pstore')
   end
