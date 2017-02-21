288d78892e3e00afcb61bd28266508a8b2d5f354
simplay <silent.simplay@gmail.com>
2017-02-21 15:08:41 +0100

    Implemented telegram command processor
    + processor requires a successful login
    + commands are prefixed by a /

--- src/telegram_bot_binding.rb
+++ src/telegram_bot_binding.rb
@@ -3,6 +3,7 @@ class TelegramBotBinding
   def initialize
     @has_terminated = false
     @token = Settings.telegram_token
+    @priviliged_users = []
   end
 
   # TODO: Use conditional locking instead of busy-wait spinning loop.
@@ -12,15 +13,17 @@ class TelegramBotBinding
         Telegram::Bot::Client.run(@token) do |bot|
           bot.listen do |message|
             case message.text
-            when '/start'
-              bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
             when /\/login/
               secret = message.text.split("/login ").last.strip
               status = Bot.login_as_telegram_user("", secret)
               status_message = status ? "Passord correct" : "Incorrect Passowrd"
               bot.api.send_message(chat_id: message.chat.id, text: status_message)
-            else
-              bot.api.send_message(chat_id: message.chat.id, text: "Sorry, I do not understand your message `#{message}`.")
+              append_user(message.from.username) if (status)
+            when /^\//
+              if user_has_signed_in?(message.from.username)
+                cmd = message.text.split(/^\//).last.strip
+                process_command(bot, message.chat.id, cmd)
+              end
             end
           end
         end
@@ -30,8 +33,38 @@ class TelegramBotBinding
     end
   end
 
+  # Shut this thread down.
   def shutdown
     @has_terminated = true
+  end
+
+  # Checks whether a user already has signed in.
+  #
+  # @param user [String] nickanme of a successfully
+  #   signed in user. User gets appended to the list once.
+  # @return [Boolean] true if a given user aleardy has signed in, otherwise false.
+  def user_has_signed_in?(user)
+    @priviliged_users.include? user
+  end
+
+  # Add the a nickname of a signed in user
+  # to the collection of signed in users.
+  #
+  # @param user [String] nickanme of a successfully
+  #   signed in user. User gets appended to the list once.
+  def append_user(user)
+    unless user_has_signed_in?(user)
+      @priviliged_users << user
+    end
+  end
+
+  # Process the received command
+  #
+  # @param bot [TelegramBot] current telegram bot instance.
+  # @param sender_id [Integer] id of telegram chat between bot and the sender.
+  # @param cmd [String] received command
+  def process_command(bot, sender_id, cmd)
+    bot.api.send_message(chat_id: sender_id, text: "Command `#{cmd}`")
   end
 
 end
